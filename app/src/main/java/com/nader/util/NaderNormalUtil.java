578ad44de25476d3f965ea3c2e8b2100184f7786
nadermkhan <muhammadnadermahbubkhan@gmail.com>
2024-02-03 15:55:50 +0600

    Some changes

--- app/src/main/java/com/nader/util/NaderNormalUtil.java
+++ app/src/main/java/com/nader/util/NaderNormalUtil.java
@@ -3,6 +3,12 @@ package com.nader.util;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 import io.github.rosemoe.sora.text.Content;
 
 public class NaderNormalUtil {
@@ -13,5 +19,10 @@ public class NaderNormalUtil {
     }
     public void showToast(String s){
         Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
+    public static void copyFile(File src, File dst) throws IOException {
+        Path sourcePath = src.toPath();
+        Path destinationPath = dst.toPath();
+        Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
     }
 }
